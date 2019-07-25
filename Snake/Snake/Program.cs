bd889a11695ffdf540521de4b782f3a31401af66
caIIIka17 <17timcarter17@gmail.com>
2019-07-25 16:01:22 +0600

    Update Program.cs

--- Snake/Snake/Program.cs
+++ Snake/Snake/Program.cs
@@ -11,20 +11,10 @@ namespace Snake
         static void Main(string[] args)
         {
 
-            Point p1 = new Point();
-            p1.x = 1;
-            p1.y = 3;
-            p1.sym = '*';
+            Point p1 = new Point(1, 3, '*');
             p1.Draw();
-            //Draw(p1.x, p1.y, p1.sym);
-
+        
             Console.ReadLine();
         }
-
-        /*static void Draw(int x, int y, char sym)
-        {
-            Console.SetCursorPosition(x, y);
-            Console.Write(sym);
-        }*/
     }
 }
